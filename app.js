780b484ba24fed7452e0f8fb49f2beec3bf76ff5
horlamesor <luisorlandogomezjimenez@gmail.com>
2019-05-25 15:58:22 -0500

    new file:   javaScriptObreily/index.html 	new file:   javaScriptObreily/main.css 	new file:   javaScriptObreily/main.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var landmark = [];
+landmark.push("my house");
+landmark.push("Front path");
+landmark.push("Flickering streetlamp");
+landmark.push("Leaky fire hydrant");
+landmark.push("Fire station");
+landmark.push("Cat rescue center");
+landmark.push("my old school");
+landmark.push("My friend´s house");
